8086bf92d7d09c03b8970caf12787a381d38a60d
Logan Kloft <lkloft@outlook.com>
2022-02-21 13:06:38 -0800

    Set up the project up to but not including step 1.

---
+++ Cpts321-assignments/Spreadsheet_Logan_Kloft/Program.cs
@@ -0,0 +1,29 @@
+﻿// <copyright file="Program.cs" company="Logan Kloft 11728076">
+// Copyright (c) Logan Kloft 11728076. All rights reserved.
+// </copyright>
+
+namespace Spreadsheet_Logan_Kloft
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using System.Windows.Forms;
+
+    /// <summary>
+    /// Houses the starting point of the application.
+    /// </summary>
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        private static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
