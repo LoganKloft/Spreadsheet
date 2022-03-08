bb8fa9cf16cb6810441ec6bdc2d17937912a7e04
Logan Kloft <lkloft@outlook.com>
2022-03-08 13:33:27 -0800

    TDD. Created skeleton factory class for BinaryOperatorNodes. Created associated test cases for the factory.

---
+++ Cpts321-assignments/SpreadsheetEngine/BinaryOperatorNodeFactory.cs
@@ -0,0 +1,22 @@
+﻿namespace CptS321
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    /// <summary>
+    /// Class that controls the creation of BinaryOperatorNodes.
+    /// </summary>
+    public class BinaryOperatorNodeFactory
+    {
+        /// <summary>
+        /// Given the character representation of an operator, returns the corresponding BinaryOperatorNode.
+        /// </summary>
+        /// <param name="op"> The character represenatation of a binary operator. </param>
+        /// <returns> A child of the BinaryOperatorNode class. </returns>
+        public static CptS321.BinaryOperatorNode CreateBinaryOperatorNode(char op)
+        {
+            return null;
+        }
+    }
+}
---
+++ Cpts321-assignments/SpreadsheetEngineTests/BinaryOperatorNodeFactoryTests.cs
@@ -0,0 +1,87 @@
+﻿namespace SpreadsheetEngineTests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using NUnit.Framework;
+
+    /// <summary>
+    /// Class that contains the tests for the BinaryOperatorNodeFactory class.
+    /// </summary>
+    public class BinaryOperatorNodeFactoryTests
+    {
+        /// <summary>
+        /// Tests the creation of an AdditionNode.
+        /// </summary>
+        [Test]
+        public void TestCreateBinaryOperatorNodeAddition()
+        {
+            object testNode = CptS321.BinaryOperatorNodeFactory.CreateBinaryOperatorNode('+');
+            bool passed = false;
+            if (testNode is CptS321.AdditionNode)
+            {
+                passed = true;
+            }
+
+            Assert.True(passed); // normal
+        }
+
+        /// <summary>
+        /// Tests the creation of an SubtractionNode.
+        /// </summary>
+        [Test]
+        public void TestCreateBinaryOperatorNodeSubtraction()
+        {
+            object testNode = CptS321.BinaryOperatorNodeFactory.CreateBinaryOperatorNode('-');
+            bool passed = false;
+            if (testNode is CptS321.SubtractionNode)
+            {
+                passed = true;
+            }
+
+            Assert.True(passed); // normal
+        }
+
+        /// <summary>
+        /// Tests the creation of an MultiplicationNode.
+        /// </summary>
+        [Test]
+        public void TestCreateBinaryOperatorNodeMultiplication()
+        {
+            object testNode = CptS321.BinaryOperatorNodeFactory.CreateBinaryOperatorNode('*');
+            bool passed = false;
+            if (testNode is CptS321.MultiplicationNode)
+            {
+                passed = true;
+            }
+
+            Assert.True(passed); // normal
+        }
+
+        /// <summary>
+        /// Tests the creation of an DivisionNode.
+        /// </summary>
+        [Test]
+        public void TestCreateBinaryOperatorNodeDivision()
+        {
+            object testNode = CptS321.BinaryOperatorNodeFactory.CreateBinaryOperatorNode('/');
+            bool passed = false;
+            if (testNode is CptS321.DivisionNode)
+            {
+                passed = true;
+            }
+
+            Assert.True(passed); // normal
+        }
+
+        /// <summary>
+        /// Test that uses a non-existing BinaryOperator.
+        /// </summary>
+        [Test]
+        public void TestCreateBinaryOperatorNodeBadOperator()
+        {
+            object testNode = CptS321.BinaryOperatorNodeFactory.CreateBinaryOperatorNode('a');
+            Assert.Null(testNode); // edge
+        }
+    }
+}
